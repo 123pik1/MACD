5777957f74cb34de4c0aa5fb8b4f95d2ee9ad437
pik <piotrkacz12@wp.pl>
2025-03-01 11:35:01 +0100

    Refactor EMA calculation and add subArrays function for MACD computation

--- MACD.py
+++ MACD.py
@@ -7,20 +7,26 @@ def toFloats(array):
         float_array.append(item)
     return float_array
 
-
 def alphaCalc(N):
     return 2 / (N + 1)
 
+def subArrays(arr1, arr2):
+    resultArr = []
+    length = len(arr1)
+    if len(arr2)<length:
+        length = len(arr2)
+    for i in range(0, length):
+        resultArr.append(arr1[i]-arr2[i])
+    return resultArr
 
 def EMA_element_calc(data, N, nmb_of_curr_elem):
     alpha=alphaCalc(N)
-    emaNumerator = 0
-    emaDenominator =0
-    for j in range(0,nmb_of_curr_elem):
-        emaNumerator += ((1-alpha)**j)*data[i-j]
+    emaNumerator = data[nmb_of_curr_elem]
+    emaDenominator =1
+    for j in range(1,nmb_of_curr_elem):
+        emaNumerator += ((1-alpha)**j)*data[nmb_of_curr_elem-j]
         emaDenominator += (1-alpha)**j
     return emaNumerator/emaDenominator
-
 
 def EMA_calc(data, N):
     EMAs_array = []
@@ -39,3 +45,6 @@ data_for_calc = data_from_csv['Price']
 data_for_calc = toFloats(data_for_calc)
 
 EMA12 = EMA_calc(data_for_calc, 12)
+EMA26 = EMA_calc(data_for_calc,26)
+MACD = subArrays(EMA12,EMA26)
+SIGNAL = EMA_calc(MACD,9)
